c01468ad0fa78c450ca1bebf64529f19d3f3a9a2
Adam Martula <martulaadam@gmail.com>
2026-06-10 18:07:31 +0200

    Adjust window layout

--- MainWindow.cs
+++ MainWindow.cs
@@ -20,18 +20,11 @@ namespace TuxDDS
             builder.Autoconnect(this);
 
             DeleteEvent += Window_DeleteEvent;
-            _button1.Clicked += Button1_Clicked;
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
-        }
-
-        private void Button1_Clicked(object sender, EventArgs a)
-        {
-            _counter++;
-            _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
         }
     }
 }
