169ba4c7b71dd6f38fe47b1862c06e6149d57459
Adam Martula <martulaadam@gmail.com>
2026-06-12 08:31:24 +0200

    Update GUI

--- Gui.cs
+++ Gui.cs
@@ -1,3 +1,4 @@
+using System;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 
@@ -5,26 +6,56 @@ namespace TuxDDS
 {
     internal class Gui : Window
     {
-        [UI] private Statusbar sbApplicationStatus;
+        [UI] private Statusbar _sbApplicationStatus;
+        private uint _statusBarContextId;
         
-        public Gui() : this(new Builder("Gui.glade"))
-        {
-        }
+        private string loadedDdsImage = null;
+        
+        public Gui() : this(new Builder("Gui.glade")) {}
 
         private Gui(Builder builder) : base(builder.GetRawOwnedObject("Gui"))
         {
             builder.Autoconnect(this);
-
-            DeleteEvent += Window_DeleteEvent;
             
+            DeleteEvent += Window_DeleteEvent;
+
             // Set a debug message for the status bar
-            var statusBarContextId = sbApplicationStatus.GetContextId("main");
-            sbApplicationStatus.Push(statusBarContextId, "No status.");
+            _statusBarContextId = _sbApplicationStatus.GetContextId("main");
+            UpdateApplicationStatus("No status.");
         }
 
-        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        private static void Window_DeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
+        }
+
+        private void UpdateApplicationStatus(string message)
+        {
+            _sbApplicationStatus.Push(_statusBarContextId, message);
+        }
+
+        private void OpenDdsImage(object sender, EventArgs args)
+        {
+            // Create a file chooser dialog
+            using var fileChooserDialog = new FileChooserDialog(
+                "Select a DDS Image",
+                this,
+                FileChooserAction.Open,
+                "Cancel", ResponseType.Cancel,
+                "Open", ResponseType.Accept);
+            
+            // Add a filter for .DDS files
+            var fileFilter = new FileFilter();
+            fileFilter.Name = "DDS Image";
+            fileFilter.AddPattern("*.[Dd][Dd][Ss]");
+            fileChooserDialog.AddFilter(fileFilter);
+            
+            // Show the dialog
+            if (fileChooserDialog.Run() == (int)ResponseType.Accept)
+            {
+                loadedDdsImage = fileChooserDialog.Filename;
+                // Load the DDS file here
+            }
         }
     }
 }
