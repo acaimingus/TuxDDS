991abd78d6d72d042d4c926c7c268948b1b2bf4b
Adam Martula <martulaadam@gmail.com>
2026-06-27 11:37:40 +0200

    Use the correct directories for storing shared libraries

--- TuxDdsGui/App.axaml.cs
+++ TuxDdsGui/App.axaml.cs
@@ -13,22 +13,6 @@ public partial class App : Application
 {
     public override void Initialize()
     {
-        // Load the wrapper library
-        NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), (libraryName, _, _) =>
-        {
-            if (libraryName == "DirectXTexWrapper")
-            {
-                var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-                var libraryPath = Path.Combine(baseDirectory, "lib", "libDirectXTexWrapper.so");
-
-                if (File.Exists(libraryPath))
-                {
-                    return NativeLibrary.Load(libraryPath);
-                }
-            }
-            return IntPtr.Zero;
-        });
-        
         AvaloniaXamlLoader.Load(this);
     }
 
